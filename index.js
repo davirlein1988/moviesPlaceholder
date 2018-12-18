cb125e1539c9455764185b16d6eba18891c2c8d0
davirlein1988 <lein.davir@gmail.com>
2018-12-17 23:25:16 -0500

    Created route for get request for a single genre and for all genres

--- index.js
+++ index.js
@@ -10,7 +10,21 @@ const genres = [
     {id: 3, genre: 'drama'}
 ]
 
+app.get('/', (req, res) => {
+    res.send("Hello, this is the root for movie services");
+});
+//Get all genres
 
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
+
+//Get a genre by id
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("The genre with given ID was not found");
+    res.send(genre);
+} );
 
 
 //Port
